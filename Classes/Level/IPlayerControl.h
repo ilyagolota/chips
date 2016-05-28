24348839988cd0430294be7d5964b37b470a5d7f
Ilya Golota <ironjava@gmail.com>
2016-05-28 14:22:19 +0300

    Character control implemented.

---
+++ Classes/Level/IPlayerControl.h
@@ -0,0 +1,13 @@
+#ifndef _CHIPS_CHALLENGE_I_PLAYER_CONTROL_H_
+#define _CHIPS_CHALLENGE_I_PLAYER_CONTROL_H_
+
+#include <Tiled/Direction.h>
+
+class IPlayerControl
+{
+public:
+	virtual bool isPressed() = 0;
+	virtual Direction getSelectedDirection() = 0;
+};
+
+#endif
