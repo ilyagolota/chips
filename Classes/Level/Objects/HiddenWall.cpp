02b0ffe0efa6a0c4c4dead2ee2e053623c865e3e
Ilya Golota <ilyagolota2@gmail.com>
2016-09-23 17:16:09 +0300

    Hidden walls: logic.

--- Classes/Level/Objects/HiddenWall.cpp
+++ Classes/Level/Objects/HiddenWall.cpp
@@ -1,5 +1,5 @@
 #include "HiddenWall.h"
-#include "Level/Level.h"
+#include <Level/Level.h>
 
 HiddenWall* HiddenWall::create(Level* level, const cocos2d::Vec2& coordinate, bool permanent)
 {
@@ -12,13 +12,80 @@ HiddenWall::HiddenWall(Level* level, const cocos2d::Vec2& coordinate, bool perma
 {
 	_permanent = permanent;
 	_known = false;
+    
+    _floor = cocos2d::Sprite::createWithSpriteFrameName("floor.png");
+    _floor->setAnchorPoint(cocos2d::Vec2::ZERO);
+    _floor->setPosition(_level->getProjector()->coordinateToPoint(_coordinate) + cocos2d::Vec2(0, -12));
+    _floor->setLocalZOrder(_level->getProjector()->coordinateToZOrder(_coordinate) + Level::BACK_Z_ORDER);
+    _level->getStage()->addChild(_floor);
+    
+    _level->getPhysicsWorld()->setBody(_coordinate, TileBody::OUTER_BOX, 7);
 }
 
-/*void HiddenWall::buildNodes()
+bool HiddenWall::isOpenableBy(const Creature *creature, Direction direction) const
 {
-    _level->setCollision(_coordinate, 0xFFFFFF);
-    _level->getSoundWorld()->setSoundSource(_coordinate, );
+    return (!_known && creature->getType() == CreatureType::CHIP);
+}
+
+void HiddenWall::beforeEnter(Creature* creature)
+{
+    _known = true;
     
-    _sprite->setPosition(_level->coordinateToPosition());
-    _level->getStage()->addChild(_sprite);
-}*/
+    auto duration = _level->getTurnDuration() * creature->getTurnsPerMove();
+    
+    auto back = inverse(creature->getDirection());
+    auto prevCoordinate = _coordinate + toVec2(back);
+    auto prevObject = _level->getObjectAt(prevCoordinate);
+    if (prevObject != nullptr)
+    {
+        auto cachedDirection = creature->getDirection();
+        creature->setDirection(back);
+        prevObject->beforeEnter(creature);
+        creature->setDirection(cachedDirection);
+    }
+    
+    creature->getSprite()->runAction(cocos2d::Sequence::create(
+        cocos2d::DelayTime::create(0.51f * duration),
+        cocos2d::CallFunc::create([this, creature, duration, prevCoordinate]()
+        {
+            auto targetPosition = _level->getProjector()->coordinateToPoint(prevCoordinate);
+            auto action = cocos2d::MoveTo::create(0.49f * duration, targetPosition);
+            
+            auto creatureSprite = creature->getSprite();
+            creatureSprite->stopActionByTag(Creature::MOVE_ACTION_TAG);
+            action->setTag(Creature::MOVE_ACTION_TAG);
+            creatureSprite->runAction(action);
+        }),
+        nullptr
+    ));
+    
+    int shape = _level->getWallShape(_coordinate);
+    char spriteFrameName[14];
+    sprintf(spriteFrameName, "wall-%04d.png", shape + 1);
+    
+    auto wallSprite = cocos2d::Sprite::createWithSpriteFrameName(spriteFrameName);
+    wallSprite->setAnchorPoint(cocos2d::Vec2::ZERO);
+    wallSprite->setPosition(cocos2d::Vec2(0, 0));
+    wallSprite->setOpacity(0);
+    wallSprite->runAction(cocos2d::Sequence::create(
+        cocos2d::DelayTime::create(0.51f * duration),
+        cocos2d::FadeTo::create(_level->getTurnDuration() * 0.35, 255),
+        nullptr
+    ));
+    _floor->addChild(wallSprite);
+}
+
+void HiddenWall::afterEnter(Creature* creature)
+{
+    auto back = inverse(creature->getDirection());
+    creature->setDirection(back);
+    
+    auto prevCoordinate = _coordinate + toVec2(back);
+    creature->setCoordinate(prevCoordinate);
+    
+    auto prevObject = _level->getObjectAt(prevCoordinate);
+    if (prevObject != nullptr)
+    {
+        prevObject->afterEnter(creature);
+    }
+}
--- Classes/Level/Objects/HiddenWall.h
+++ Classes/Level/Objects/HiddenWall.h
@@ -10,10 +10,16 @@ public:
 	static HiddenWall* create(Level* level, const cocos2d::Vec2& coordinate, bool permanent);
     
 	HiddenWall(Level* level, const cocos2d::Vec2& coordinate, bool permanent);
-
+    bool isOpenableBy(const Creature *creature, Direction direction) const override;
+    void beforeEnter(Creature* creature) override;
+    void afterEnter(Creature* creature) override;
+    
 private:
 	bool _permanent;
 	bool _known;
+    
+    cocos2d::Sprite* _floor;
+    cocos2d::Sprite* _wall;
 };
 
 #endif
