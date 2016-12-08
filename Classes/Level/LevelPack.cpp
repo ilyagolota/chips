781c490821cb9f1a72cc880457f18bcedfae4d9e
Ilya Golota <ilyagolota2@gmail.com>
2016-12-08 15:52:56 +0300

    Optimized level pack reader.

--- Classes/Level/LevelPack.cpp
+++ Classes/Level/LevelPack.cpp
@@ -1,7 +1,6 @@
 #include <fstream>
 
 #include "LevelPack.h"
-#include <Utils/BinaryReader.h>
 #include <Utils/MD5.h>
 
 LevelPack* LevelPack::create(const std::string& filename, const std::vector<char>& hash)
@@ -18,19 +17,25 @@ LevelPack::LevelPack(const std::string& filename, const std::vector<char>& hash)
     
     auto path = cocos2d::FileUtils::getInstance()->fullPathForFilename(_filename);
     std::fstream stream(path, std::ios::binary | std::ios::in);
-    utils::BinaryReader reader(&stream);
     
-    int signature = reader.readInt32();
+    auto buf = stream.rdbuf();
+    
+    int signature;
+    buf->sgetn((char*)&signature, 4);
     CC_UNUSED_PARAM(signature);
     
-    size_t totalLevelCount = static_cast<size_t>(reader.readInt16());
+    short totalLevelCount;
+    buf->sgetn((char*)&totalLevelCount, 2);
     
     _entries.reserve(totalLevelCount);
     for (int levelIndex = 0; levelIndex < totalLevelCount; levelIndex++)
     {
         LevelPackEntry entry;
-        entry.size = reader.readUInt16();
-        entry.offset = reader.getBaseStream().tellg();
+        
+        entry.size = 0;
+        buf->sgetn((char*)&entry.size, 2);
+        
+        entry.offset = buf->pubseekoff(0, std::ios::cur);
         
         struct
         {
@@ -39,33 +44,38 @@ LevelPack::LevelPack(const std::string& filename, const std::vector<char>& hash)
             short int chipsRequired;
             short int fieldCount;
         } params;
-        reader.read((char *)&params, sizeof params);
+        buf->sgetn((char*)&params, 8);
         
         const int LAYER_COUNT = 2;
         for (int j = 0; j < LAYER_COUNT; j++)
         {
-            int layerSize = reader.readUInt16();
-            reader.getBaseStream().seekg(layerSize, std::ios::cur);
+            unsigned short layerSize;
+            buf->sgetn((char*)&layerSize, 2);
+            buf->pubseekoff(layerSize, std::ios::cur);
         }
         
-        while (reader.getBaseStream().tellg() < entry.offset + entry.size)
+        while (buf->pubseekoff(0, std::ios::cur) < entry.offset + entry.size)
         {
-            unsigned int fieldType = reader.readUInt8();
-            unsigned int fieldSize = reader.readUInt8();
+            unsigned char fieldType;
+            buf->sgetn((char*)&fieldType, 1);
+            
+            unsigned char fieldSize;
+            buf->sgetn((char*)&fieldSize, 1);
+            
             if (fieldType != 3)
             {
-                reader.getBaseStream().seekg(fieldSize, std::ios::cur);
+                buf->pubseekoff(fieldSize, std::ios::cur);
             }
             else
             {
                 std::string title(fieldSize, '\0');
-                reader.read(&title[0], fieldSize);
+                buf->sgetn((char*)&title[0], fieldSize);
                 entry.title = title;
                 break;
             }
         }
         
-        reader.getBaseStream().seekg(entry.offset + entry.size, std::ios::beg);
+        buf->pubseekoff(entry.offset + entry.size, std::ios::beg);
         _entries.push_back(std::move(entry));
     }
     
@@ -81,6 +91,11 @@ LevelConfig* LevelPack::readLevelConfig(size_t entryIndex)
 {
     auto path = cocos2d::FileUtils::getInstance()->fullPathForFilename(_filename);
     std::fstream stream(path, std::ios::binary | std::ios::in);
+    std::istream::sentry sen(stream);
+    if (!sen)
+    {
+        throw std::runtime_error("Level pack is corrupted");
+    }
     
     utils::MD5 md5;
     md5.update(stream);
@@ -90,27 +105,42 @@ LevelConfig* LevelPack::readLevelConfig(size_t entryIndex)
         throw std::runtime_error("Level pack is corrupted");
     }
     
-    auto& entry = _entries[entryIndex];
-    stream.seekg(entry.offset, std::ios::beg);
+    auto& levelEntry = _entries[entryIndex];
     
-    utils::BinaryReader reader(&stream);
+    auto buf = stream.rdbuf();
+    buf->pubseekoff(levelEntry.offset, std::ios::beg);
+    
     auto* levelConfig = LevelConfig::create();
     
-    levelConfig->setNumber(reader.readUInt16());
-    levelConfig->setTime(reader.readUInt16());
-    levelConfig->setChipsRequired(reader.readUInt16());
+    short number;
+    buf->sgetn((char*)&number, 2);
+    levelConfig->setNumber(number);
     
-    int fieldCount = reader.readUInt16();
+    short time;
+    buf->sgetn((char*)&time, 2);
+    levelConfig->setTime(time);
+    
+    short chipsRequired;
+    buf->sgetn((char*)&chipsRequired, 2);
+    levelConfig->setChipsRequired(chipsRequired);
+    
+    short fieldCount;
+    buf->sgetn((char*)&fieldCount, 2);
     CC_UNUSED_PARAM(fieldCount);
     
-	const int LAYER_COUNT = 2;
-	for (int fieldIndex = 0; fieldIndex < LAYER_COUNT; fieldIndex++)
+    const int LAYER_COUNT = 2;
+    for (int fieldIndex = 0; fieldIndex < LAYER_COUNT; fieldIndex++)
     {
         std::vector<TileType> layer(LevelConfig::LAYER_SIZE);
-        short size = reader.readInt16();
+        
+        short size;
+        buf->sgetn((char*)&size, 2);
+        
         for (short i = 0, k = 0; i < size; i++)
         {
-            unsigned char byte1 = reader.readUInt8();
+            unsigned char byte1;
+            buf->sgetn((char*)&byte1, 1);
+            
             if (byte1 != 255)
             {
                 layer[k] = static_cast<TileType>(byte1);
@@ -118,8 +148,12 @@ LevelConfig* LevelPack::readLevelConfig(size_t entryIndex)
             }
             else
             {
-                unsigned char count = reader.readUInt8();
-                unsigned char byte2 = reader.readUInt8();
+                unsigned char count;
+                buf->sgetn((char*)&count, 1);
+                
+                unsigned char byte2;
+                buf->sgetn((char*)&byte2, 1);
+                
                 for (unsigned char j = 0; j < count; j++)
                 {
                     layer[k] = static_cast<TileType>(byte2);
@@ -131,26 +165,35 @@ LevelConfig* LevelPack::readLevelConfig(size_t entryIndex)
         levelConfig->getLayers().push_back(std::move(layer));
     }
     
-    size_t position = reader.getBaseStream().tellg();
-    size_t end = position + reader.readInt16();
+    std::streamsize position = buf->pubseekoff(0, std::ios::cur);
+    std::streamsize end = 0;
+    buf->sgetn((char*)&end, 2);
+    end += position;
     
     while (position < end)
     {
-        short fieldType = reader.readUInt8();
-        short fieldSize = reader.readUInt8();
+        unsigned char fieldType;
+        buf->sgetn((char*)&fieldType, 1);
+        
+        unsigned char fieldSize;
+        buf->sgetn((char*)&fieldSize, 1);
         
         if (fieldType == 1)
         {
-            levelConfig->setTime(reader.readUInt16());
+            short time;
+            buf->sgetn((char*)&time, 2);
+            levelConfig->setTime(time);
         }
         else if (fieldType == 2)
         {
-            levelConfig->setChipsRequired(reader.readUInt16());
+            short chipsRequired;
+            buf->sgetn((char*)&chipsRequired, 2);
+            levelConfig->setChipsRequired(chipsRequired);
         }
         else if (fieldType == 3)
         {
             std::string title(fieldSize - 1, '\0');
-            reader.read(&title[0], fieldSize);
+            buf->sgetn(&title[0], fieldSize);
             levelConfig->setTitle(title);
         }
         else if (fieldType == 4)
@@ -158,9 +201,8 @@ LevelConfig* LevelPack::readLevelConfig(size_t entryIndex)
             int trapCount = fieldSize / 10;
             for (int i = 0; i < trapCount; i++)
             {
-                short wireData[4];
-                reader.read((char*)&wireData, sizeof wireData);
-                reader.readUInt16();
+                short wireData[5];
+                buf->sgetn((char*)wireData, fieldSize);
                 
                 LevelWire wire;
                 wire.startCoordinate = cocos2d::Vec2(wireData[0], wireData[1]);
@@ -174,8 +216,8 @@ LevelConfig* LevelPack::readLevelConfig(size_t entryIndex)
             for (int i = 0; i < clonerCount; i++)
             {
                 short wireData[4];
-                reader.read((char*)&wireData, sizeof wireData);
-
+                buf->sgetn((char*)wireData, fieldSize);
+                
                 LevelWire wire;
                 wire.startCoordinate = cocos2d::Vec2(wireData[0], wireData[1]);
                 wire.endCoordinate = cocos2d::Vec2(wireData[2], wireData[3]);
@@ -184,14 +226,14 @@ LevelConfig* LevelPack::readLevelConfig(size_t entryIndex)
         }
         else if (fieldType == 6)
         {
-            std::string password(fieldSize, '\0');
-            reader.read(&password[0], fieldSize);
+            std::string password(fieldSize - 1, '\0');
+            buf->sgetn(&password[0], fieldSize);
             levelConfig->setPassword(password);
         }
         else if (fieldType == 7)
         {
-            std::string hint(fieldSize, '\0');
-            reader.read(&hint[0], fieldSize);
+            std::string hint(fieldSize - 1, '\0');
+            buf->sgetn(&hint[0], fieldSize);
             levelConfig->setHint(hint);
         }
         else if (fieldType == 10)
@@ -201,8 +243,8 @@ LevelConfig* LevelPack::readLevelConfig(size_t entryIndex)
              var y = input.getUint8();*/
         }
         
-        position += fieldSize + sizeof(unsigned char) + sizeof(unsigned char);
-        reader.getBaseStream().seekg(position, std::ios::beg);
+        position += fieldSize + sizeof(char) + sizeof(char);
+        buf->pubseekoff(position, std::ios::beg);
     }
     
     stream.close();
